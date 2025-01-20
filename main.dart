8d36b80460a998a97f2daf5365d354ca06614734
hansmeuller <118677299+hansmeuller@users.noreply.github.com>
2025-01-20 14:42:41 +0100

    update

--- main.dart
+++ main.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
-import 'logic.dart'; // Importiere die Logikdatei
+import 'logic.dart';
 
 void main() {
   runApp(const ScanRenameApp());
+}
+
+void main() {
+  runApp(const ScanRenameApp());
+}
+
+class ScanRenameApp extends StatelessWidget {
+  const ScanRenameApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: const ScanRenameScreen(),
+    );
+  }
 }
