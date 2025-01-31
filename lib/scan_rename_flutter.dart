e2960641d0f200830c239aa0bb660d7e91d80d30
hansmeuller <118677299+hansmeuller@users.noreply.github.com>
2025-01-31 23:44:35 +0100

    update

--- lib/scan_rename_flutter.dart
+++ lib/scan_rename_flutter.dart
@@ -3,8 +3,10 @@ import 'package:pdf_render/pdf_render.dart';
 import 'package:path/path.dart' as path;
 import 'package:intl/intl.dart';
 import 'dart:async';
+import 'dart:typed_data';
+import 'package:image/image.dart' as img;
 
-// scanbereiche
+// Definition der Scanbereiche in cm
 const double windowLeftCm = 2.0;
 const double windowTopCm = 4.5;
 const double windowWidthCm = 9.0;
@@ -13,19 +15,19 @@ const double windowHeightCm = 4.5;
 const double knickfalteTopCm = 10.0;
 const double knickfalteHeightCm = 2.0;
 
-// keywords
+// Keywords zur Erkennung von Aktenzeichen
 const List<String> aktenzeichenKeywords = ["Bitte bei"];
 const List<String> ausschlussListe = ["Postfach", "PLZ", "Postzentrum"];
 const Map<String, String> ocrCorrections = {"unaedeckte": "ungedeckte"};
 
-// log func
+// Logging-Funktion
 void logMessage(String message) {
   final timestamp = DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.now());
   final logFile = File('Logeinträge.txt');
   logFile.writeAsStringSync('$timestamp - $message\n', mode: FileMode.append);
 }
 
-// temp löschen
+// Temporäre PNG löschen
 void deleteTempPng(String filePath) {
   final file = File(filePath);
   if (file.existsSync()) {
@@ -34,33 +36,49 @@ void deleteTempPng(String filePath) {
   }
 }
 
-// rule
+// Generelle Regel: Maximal ein Leerzeichen zwischen Wörtern
 String normalizeSpacing(String text) {
   return text.replaceAll(RegExp(r'\s{2,}'), ' ').trim();
 }
 
-// auf kto prüfen
+// Kontoauszug prüfen
 bool isKontoauszug(PdfPageImage image) {
-  return image.height < 1500; // Bedingung für Kontoauszug
+  return image.height < 1500;
 }
 
-// text extrahieren
-Future<List<String>> extractTextFromWindow(PdfPageImage image, double dpi, String fileName, double topCm, double heightCm) async {
+// Eindeutigen Dateinamen generieren
+String getUniqueFilename(String filePath) {
+  var base = path.withoutExtension(filePath);
+  var ext = path.extension(filePath);
+  var counter = 1;
+  var newPath = filePath;
+  while (File(newPath).existsSync()) {
+    newPath = "$base ($counter)$ext";
+    counter++;
+  }
+  return newPath;
+}
+
+// Text aus definiertem Fenster extrahieren
+Future<List<String>> extractTextFromWindow(Uint8List imageBytes, double dpi, String fileName, double topCm, double heightCm) async {
   try {
+    final img.Image? image = img.decodeImage(imageBytes);
+    if (image == null) {
+      logMessage('Fehler beim Dekodieren des Bildes');
+      return [];
+    }
     final pixelsPerCm = dpi / 2.54;
     final left = (windowLeftCm * pixelsPerCm).toInt();
     final right = ((windowLeftCm + windowWidthCm) * pixelsPerCm).toInt();
     final top = (topCm * pixelsPerCm).toInt();
     final bottom = ((topCm + heightCm) * pixelsPerCm).toInt();
 
-    final croppedImage = image.subImage(Rect.fromLTWH(left.toDouble(), top.toDouble(), (right - left).toDouble(), (bottom - top).toDouble()));
+    final croppedImage = img.copyCrop(image, left, top, right - left, bottom - top);
     final tempPngPath = '$fileName_window_preview.png';
-
-    final tempFile = File(tempPngPath);
-    await tempFile.writeAsBytes(croppedImage.bytes);
+    File(tempPngPath).writeAsBytesSync(img.encodePng(croppedImage));
     logMessage('Fensterbereich gespeichert: $tempPngPath');
 
-    final result = await Process.run('python3', ['-m', 'easyocr', tempPngPath]);
+    final result = await Process.run('python', ['assets/python/ocr.py', tempPngPath]);
     deleteTempPng(tempPngPath);
 
     if (result.exitCode == 0) {
@@ -76,47 +94,19 @@ Future<List<String>> extractTextFromWindow(PdfPageImage image, double dpi, Strin
   }
 }
 
-// abs extrahieren
-String extractSender(List<String> results) {
-  if (results.isNotEmpty) {
-    var firstLine = results.first;
-    for (var exclusion in ausschlussListe) {
-      firstLine = firstLine.replaceAll(exclusion, '');
-    }
-    firstLine = normalizeSpacing(firstLine);
-    return 'Absender: $firstLine';
-  }
-  return 'Kein Absender gefunden';
-}
-
-// az extrahieren
-String? extractCaseNumber(List<String> results) {
-  for (var idx = 0; idx < results.length; idx++) {
-    var lineText = normalizeSpacing(results[idx]);
-
-    for (var correction in ocrCorrections.entries) {
-      lineText = lineText.replaceAll(correction.key, correction.value);
-    }
-
-    if (aktenzeichenKeywords.any((keyword) => lineText.startsWith(keyword))) {
-      logMessage('Keyword für Aktenzeichen gefunden: $lineText');
-
-      if (idx + 3 < results.length) {
-        final aktenzeichenLine = normalizeSpacing(results[idx + 3]);
-        return aktenzeichenLine.substring(0, 13);
-      }
-    }
-  }
-  return null;
-}
-
-// woerk
+// PDF verarbeiten
 Future<void> processPdf(String filePath) async {
   try {
     final fileName = path.basename(filePath);
     final document = await PdfDocument.openFile(filePath);
     final page = await document.getPage(1);
     final image = await page.render();
+    final imageBytes = image?.bytes;
+
+    if (imageBytes == null) {
+      logMessage('Fehler beim Rendern der PDF-Seite');
+      return;
+    }
 
     if (isKontoauszug(image!)) {
       logMessage('Dokument erkannt als Kontoauszug: $filePath');
@@ -124,18 +114,24 @@ Future<void> processPdf(String filePath) async {
       return;
     }
 
-    final senderResults = await extractTextFromWindow(image, 300, fileName, windowTopCm, windowHeightCm);
-    final sender = extractSender(senderResults);
+    final senderResults = await extractTextFromWindow(imageBytes, 300, fileName, windowTopCm, windowHeightCm);
+    final sender = senderResults.isNotEmpty ? senderResults.first : "Unbekannter Absender";
     logMessage('Gefundener Absender in $filePath: $sender');
 
-    final subjectOrCase = extractCaseNumber(senderResults);
-    logMessage('Gefundener Betreff oder Aktenzeichen in $filePath: ${subjectOrCase ?? "Kein relevanter Eintrag gefunden"}');
+    final caseNumberResults = await extractTextFromWindow(imageBytes, 300, fileName, knickfalteTopCm, knickfalteHeightCm);
+    final subjectOrCase = caseNumberResults.isNotEmpty ? caseNumberResults.first : "Kein relevanter Eintrag gefunden";
+    logMessage('Gefundener Betreff oder Aktenzeichen in $filePath: $subjectOrCase');
+
+    final newFileName = normalizeSpacing('$sender_$subjectOrCase.pdf').replaceAll('__', '_');
+    final newFilePath = getUniqueFilename(path.join(path.dirname(filePath), newFileName));
+    File(filePath).renameSync(newFilePath);
+    logMessage('Datei umbenannt: $filePath -> $newFilePath');
   } catch (e) {
     logMessage('Fehler bei der Verarbeitung von $filePath: $e');
   }
 }
 
-// pdf verarbeiten
+// PDFs im Ordner verarbeiten
 void processPdfs(String folder) {
   final dir = Directory(folder);
   for (var file in dir.listSync()) {
@@ -146,7 +142,7 @@ void processPdfs(String folder) {
   }
 }
 
-// eintritt
+// Hauptprogramm
 void main() {
   final currentFolder = Directory.current.path;
   processPdfs(currentFolder);
